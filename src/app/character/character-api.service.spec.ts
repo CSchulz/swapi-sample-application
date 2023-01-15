f91942c06e4a91061869da17b1dda5620198956f
Christian Schulz <kontakt@schulzolutions.de>
2023-01-15 19:29:39 +0100

    some more tests

--- src/app/character/character-api.service.spec.ts
+++ src/app/character/character-api.service.spec.ts
@@ -1,27 +1,16 @@
 import {CharacterApiService} from "./character-api.service";
 import {TestBed} from "@angular/core/testing";
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
-import {ListRecord} from "../commons/rest-api";
-import {CharacterListItem} from "./model";
 import {API_URL} from "../commons/environment";
+import {
+  mockListRecordCharacterListItem,
+  mockSingleRecordCharacterDetailItem,
+  mockSingleRecordCharacterDetailItemTwo
+} from "./mocks";
 
-const testListResult: ListRecord<CharacterListItem> = {
-  message: 'ok',
-  next: null,
-  previous: null,
-  results: [
-    {
-      uid: '1',
-      name: 'earth',
-    },
-    {
-      uid: '2',
-      name: 'mars',
-    }
-  ],
-  total_pages: 1,
-  total_records: 2,
-}
+const listUrl = 'people/?page=1&limit=1000';
+const entryUrl = 'people/1'
+const entryUrlTwo = 'people/2'
 
 describe(CharacterApiService.name, () => {
   let httpTestingController: HttpTestingController;
@@ -48,12 +37,52 @@ describe(CharacterApiService.name, () => {
     httpTestingController.verify();
   });
 
-  it('return record list', () => {
+  it('should return record list', () => {
     service.getItemList().subscribe((result) => {
-      expect(result).toEqual(testListResult);
+      expect(result).toEqual(mockListRecordCharacterListItem);
     });
 
-    const request = httpTestingController.expectOne('people/?page=1&limit=1000');
-    request.flush(testListResult);
+    const request = httpTestingController.expectOne(listUrl);
+    request.flush(mockListRecordCharacterListItem);
+  });
+
+  it('should cache record list', () => {
+    service.getItemList().subscribe((result) => {
+      expect(result).toEqual(mockListRecordCharacterListItem);
+    });
+
+    const request = httpTestingController.expectOne(listUrl);
+    request.flush(mockListRecordCharacterListItem);
+
+    service.getItemList().subscribe((result) => {
+      expect(result).toEqual(mockListRecordCharacterListItem);
+    });
+    httpTestingController.expectNone(listUrl);
+  });
+
+  it('should return record entry and should cache it', () => {
+    service.getItem(1).subscribe((result) => {
+      expect(result).toEqual(mockSingleRecordCharacterDetailItem);
+    });
+
+    const request = httpTestingController.expectOne(entryUrl);
+    request.flush(mockSingleRecordCharacterDetailItem);
+
+    service.getItem(1).subscribe((result) => {
+      expect(result).toEqual(mockSingleRecordCharacterDetailItem);
+    });
+    httpTestingController.expectNone(entryUrl);
+
+    service.getItem(2).subscribe((result) => {
+      expect(result).toEqual(mockSingleRecordCharacterDetailItemTwo);
+    });
+
+    const requestTwo = httpTestingController.expectOne(entryUrlTwo);
+    requestTwo.flush(mockSingleRecordCharacterDetailItemTwo);
+
+    service.getItem(2).subscribe((result) => {
+      expect(result).toEqual(mockSingleRecordCharacterDetailItemTwo);
+    });
+    httpTestingController.expectNone(entryUrl);
   });
 })
---
+++ src/app/character/mocks.ts
@@ -0,0 +1,49 @@
+import {ListRecord, SingleRecord} from "../commons/rest-api";
+import {CharacterDetailItem, CharacterListItem} from "./model";
+
+export const mockListRecordCharacterListItem: ListRecord<CharacterListItem> = {
+  message: 'ok',
+  next: null,
+  previous: null,
+  results: [
+    {
+      uid: '1',
+      name: 'earth',
+    },
+    {
+      uid: '2',
+      name: 'mars',
+    }
+  ],
+  total_pages: 1,
+  total_records: 2,
+}
+
+export const mockCharacterDetailItem: CharacterDetailItem = {
+  uid: '1',
+  properties: {
+    height: '172',
+    mass: '77',
+    hair_color: 'blond',
+    skin_color: 'fair',
+    eye_color: 'blue',
+    birth_year: '19BBY',
+    gender: 'male',
+    name: 'Luke Skywalker',
+    homeworld: 'https://www.swapi.tech/api/planets/1',
+  }
+}
+
+export const mockSingleRecordCharacterDetailItem: SingleRecord<CharacterDetailItem> = {
+  message: 'ok',
+  result: {
+    ...mockCharacterDetailItem,
+  }
+}
+export const mockSingleRecordCharacterDetailItemTwo: SingleRecord<CharacterDetailItem> = {
+  message: 'ok',
+  result: {
+    ...mockCharacterDetailItem,
+    uid: '2',
+  }
+}
