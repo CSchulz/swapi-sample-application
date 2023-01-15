d5fee9e618aadf851685a366114e3eee5543649c
Christian Schulz <kontakt@schulzolutions.de>
2023-01-15 18:26:43 +0100

    cleanup

--- src/app/character/model.ts
+++ src/app/character/model.ts
@@ -15,9 +15,5 @@ export interface CharacterDetailItem {
     mass: string;
     skin_color: string;
     homeworld: string;
-    // films array;
-    // species array;
-    // starships array;
-    // vehicles array;
   }
 }
